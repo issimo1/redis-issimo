b6601847d164328d543bb8ac0cd94cb63d23dd75
issimo1 <2530566719@qq.com>
2024-12-03 15:19:22 +0800

    添加timewheel代码

--- utils/timewheel/struct.go
+++ utils/timewheel/struct.go
@@ -2,6 +2,7 @@ package timewheel
 
 import (
 	"container/list"
+	"github.com/issimo1/redis-issimo/utils/logger"
 	"time"
 )
 
@@ -46,4 +47,111 @@ func New(interval time.Duration, slotNum int) *TimeWheel {
 		timeWheel.slots[i] = list.New()
 	}
 	return timeWheel
+}
+
+func (tw *TimeWheel) doTask() {
+	for {
+		select {
+		case <-tw.ticker.C:
+			tw.execTask()
+		case t := <-tw.addChan:
+			tw.addTask(t)
+		case key := <-tw.cancelChan:
+			tw.cancelTask(key)
+		case <-tw.stopChan:
+		}
+	}
+}
+
+func (tw *TimeWheel) execTask() {
+	l := tw.slots[tw.curSlotPos]
+	if tw.curSlotPos == tw.slotNum-1 {
+		tw.curSlotPos = 0
+	} else {
+		tw.curSlotPos++
+	}
+	go tw.scanList(l)
+}
+
+func (tw *TimeWheel) scanList(l *list.List) {
+	for e := l.Front(); e != nil; {
+		t := e.Value.(*task)
+		if t.circle > 0 {
+			t.circle--
+			continue
+		}
+
+		go func() {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Error(err)
+				}
+			}()
+			call := t.callback
+			call()
+		}()
+
+		next := e.Next()
+		l.Remove(next)
+		if t.key != "" {
+			delete(tw.m, t.key)
+		}
+		e = next
+	}
+
+}
+
+func (tw *TimeWheel) posAndCircle(d time.Duration) (pos, circle int) {
+	delaySecond := int(d.Seconds())
+	intervalSecond := int(tw.interval.Seconds())
+	pos = (tw.curSlotPos + delaySecond/intervalSecond) % tw.slotNum
+	circle = (delaySecond / intervalSecond) / tw.slotNum
+	return
+}
+
+func (tw *TimeWheel) addTask(t *task) {
+	pos, circle := tw.posAndCircle(t.delay)
+	t.circle = circle
+
+	element := tw.slots[pos].PushBack(t)
+	if t.key != "" {
+		if _, ok := tw.m[t.key]; ok {
+
+		}
+		tw.m[t.key] = &taskPos{pos: pos, ele: element}
+	}
+}
+
+func (tw *TimeWheel) cancelTask(key string) {
+	taskPos, ok := tw.m[key]
+	if !ok {
+		return
+	}
+	tw.slots[taskPos.pos].Remove(taskPos.ele)
+	delete(tw.m, key)
+}
+
+func (tw *TimeWheel) Start() {
+	tw.ticker = time.NewTicker(tw.interval)
+	go tw.doTask()
+}
+
+func (tw *TimeWheel) Stop() {
+	tw.stopChan <- struct{}{}
+}
+
+func (tw *TimeWheel) Add(delay time.Duration, key string, call func()) {
+	if delay < 0 {
+		return
+	}
+	t := task{
+		delay:    delay,
+		key:      key,
+		callback: call,
+	}
+	tw.addChan <- &t
+}
+
+func (tw *TimeWheel) Cancel(key string) {
+	tw.cancelChan <- key
 }
--- utils/timewheel/timewheel.go
+++ utils/timewheel/timewheel.go
@@ -6,8 +6,22 @@ type Delay struct {
 	tw *TimeWheel
 }
 
-func NewDelay(interval time.Duration, slotNums int) *Delay {
-	return &Delay{
-		tw: New(interval, slotNums),
-	}
+func NewDelay() *Delay {
+	delay := &Delay{}
+	delay.tw = New(1*time.Second, 3600)
+	delay.tw.Start()
+	return delay
+}
+
+func (d *Delay) AddAt(expire time.Time, key string, call func()) {
+	interval := time.Until(expire)
+	d.Add(interval, key, call)
+}
+
+func (d *Delay) Add(expire time.Duration, key string, call func()) {
+	d.tw.Add(expire, key, call)
+}
+
+func (d *Delay) Cancel(key string) {
+	d.tw.Cancel(key)
 }
